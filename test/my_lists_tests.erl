1f93645929f0b2182002ad595b5313446f824677
Mirko Bonadei <mirko.bonadei@gmail.com>
2014-11-12 13:53:14 +0100

    Porting `my_lists_tests` to `triq`

--- test/my_lists_tests.erl
+++ test/my_lists_tests.erl
@@ -1,6 +1,6 @@
--module(my_lists_eqc).
--include_lib("eqc/include/eqc.hrl").
--define(NUMBER_OF_GENERATED_TESTS, 200).
+-module(my_lists_tests).
+-include_lib("triq/include/triq.hrl").
+-define(NUMBER_OF_GENERATED_TESTS, 100).
 -export([
          %% sum/1
          prop_sum_monotonicity/0,
@@ -8,27 +8,29 @@
          prop_sum_base_case/0
 ]).
 
+sum_test() ->
+  triq:check(prop_sum_monotonicity()).
+
 %% my_lists:sum/1
 prop_sum_monotonicity() ->
-  eqc:numtests(?NUMBER_OF_GENERATED_TESTS,
-    ?FORALL({L, NonNegInteger}, {list(nat()), nat()}, 
+    ?FORALL({L, NonNegInteger}, {list(pos_integer()), pos_integer()}, 
             begin
               L1 = [NonNegInteger|L],
               my_lists:sum(L) =< my_lists:sum(L1)
-            end)).
+            end).
 
-prop_sum_identity() ->
-  eqc:numtests(?NUMBER_OF_GENERATED_TESTS,
-    ?FORALL({L}, {list(nat())}, 
-            begin
-              L1 = [0|L],
-              eqc:equals(my_lists:sum(L), my_lists:sum(L1))
-            end)).
+%% prop_sum_identity() ->
+%%   eqc:numtests(?NUMBER_OF_GENERATED_TESTS,
+%%     ?FORALL({L}, {list(nat())}, 
+%%             begin
+%%               L1 = [0|L],
+%%               eqc:equals(my_lists:sum(L), my_lists:sum(L1))
+%%             end)).
 
-prop_sum_base_case() ->
-  eqc:numtests(?NUMBER_OF_GENERATED_TESTS,
-    ?FORALL({Int}, {int()}, 
-            begin
-              L = [Int],
-              eqc:equals(my_lists:sum(L), Int)
-            end)).
+%% prop_sum_base_case() ->
+%%   eqc:numtests(?NUMBER_OF_GENERATED_TESTS,
+%%     ?FORALL({Int}, {int()}, 
+%%             begin
+%%               L = [Int],
+%%               eqc:equals(my_lists:sum(L), Int)
+%%             end)).
