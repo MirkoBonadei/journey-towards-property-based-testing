e6e46a7f26ecd47fd84760f9af49bd6c0c8e04c1
Mirko Bonadei <mirko.bonadei@gmail.com>
2014-11-13 13:13:50 +0100

    Switched `my_lists_tests` from Erlang Quickcheck to Trifork Quickcheck

--- test/my_lists_tests.erl
+++ test/my_lists_tests.erl
@@ -1,37 +1,32 @@
 -module(my_lists_tests).
 -include_lib("triq/include/triq.hrl").
+-include_lib("eunit/include/eunit.hrl").
 -define(NUMBER_OF_GENERATED_TESTS, 100).
--export([
-         sum_test/0
-         %%prop_sum_monotonicity/0
-         %%prop_sum_identity/0,
-         %%prop_sum_base_case/0
-]).
+-export([sum_test/0]).
 
 sum_test() ->
-  triq:check(prop_sum_monotonicity()).
+  triq:check(prop_sum_monotonicity()),
+  triq:check(prop_sum_identity()),
+  triq:check(prop_sum_base_case()).
 
 %% my_lists:sum/1
 prop_sum_monotonicity() ->
-    ?FORALL({L, NonNegInteger}, {list(pos_integer()), pos_integer()}, 
+    ?FORALL({L, NonNegInteger}, {list(int()), pos_integer()},
             begin
               L1 = [NonNegInteger|L],
-              %%my_lists:sum(L) =< my_lists:sum(L1)
-              1 = 0
+              my_lists:sum(L) =< my_lists:sum(L1)
             end).
 
-%% prop_sum_identity() ->
-%%   eqc:numtests(?NUMBER_OF_GENERATED_TESTS,
-%%     ?FORALL({L}, {list(nat())}, 
-%%             begin
-%%               L1 = [0|L],
-%%               eqc:equals(my_lists:sum(L), my_lists:sum(L1))
-%%             end)).
+prop_sum_identity() ->
+    ?FORALL({L}, {list(pos_integer())}, 
+            begin
+              L1 = [0|L],
+              my_lists:sum(L) =:= my_lists:sum(L1)
+            end).
 
-%% prop_sum_base_case() ->
-%%   eqc:numtests(?NUMBER_OF_GENERATED_TESTS,
-%%     ?FORALL({Int}, {int()}, 
-%%             begin
-%%               L = [Int],
-%%               eqc:equals(my_lists:sum(L), Int)
-%%             end)).
+prop_sum_base_case() ->
+    ?FORALL({Int}, {int()}, 
+            begin
+              L = [Int],
+              my_lists:sum(L) =:= Int
+            end).
