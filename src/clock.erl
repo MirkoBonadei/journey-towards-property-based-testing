dbbb2d5e3d1f33398c7746c51bea3d15003018f4
Mirko Bonadei <mirko.bonadei@gmail.com>
2014-11-15 17:43:01 +0100

    Implementing the clock module with Triq tests
    I have only done 2 tests today but they are really different from the examples at https://erlcode.wordpress.com/tag/quickcheck/ since I am trying to be as general as possible.

--- src/clock.erl
+++ src/clock.erl
@@ -1,4 +1,16 @@
 -module(clock).
--export([hms2s/1]).
+-export([hms2s/1, s2hms/1]).
 
-hms2s({H,M,S}) -> H*60*60 + M*60 + S.
+-type time() :: {0..23, 0..59, 0..59}.
+
+-spec hms2s(time()) -> non_neg_integer().
+hms2s({H, M, S}) -> 
+  (H * 60 * 60) + (M * 60) + S.
+
+-spec s2hms(non_neg_integer()) -> time().
+s2hms(Seconds) ->
+  H = Seconds div 3600,
+  Minutes = Seconds rem 3600,
+  M = Minutes div 60,
+  S = Minutes rem 60,
+  {H, M, S}.
--- test/clock_tests.erl
+++ test/clock_tests.erl
@@ -1,10 +1,31 @@
--module(clock_eqc).
-%% -include_lib("eqc/include/eqc.hrl").
-%% -compile(export_all).
+-module(clock_tests).
+-include_lib("triq/include/triq.hrl").
 
-%% prop_time_to_seconds() ->
-%%     ?FORALL({H,M,S},{nat(),nat(),nat()},
-%%     ?IMPLIES(H < 24,
-%%     ?IMPLIES(M < 60,
-%%     ?IMPLIES(S < 60,
-%%              clock:hms2s({H,M,S}) == (H*60*60)+(M*60)+S)))).
+%% Generator (or domain since we are using Triq)
+%% A sample:
+%% [{52,23,54},
+%%  {2,28,48},
+%%  {4,33,22},
+%%  {10,35,2},
+%%  {39,34,32},
+%%  {8,53,20},
+%%  {10,4,31},
+%%  {49,5,52},
+%%  {6,40,50},
+%%  {16,52,12},
+%%  {47,8,19}]
+time_dom() ->
+  {choose(0, 23), choose(0, 59), choose(0, 59)}.
+
+%% Properties
+prop_to_seconds_conversion_always_greater_than_0() ->
+  ?FORALL(
+     {H, M, S},
+     time_dom(),
+       clock:hms2s({H, M, S}) >= 0).
+
+prop_to_seconds_conversion_inverse() ->
+  ?FORALL(
+     {H, M, S},
+     time_dom(),
+         {H, M, S} =:= clock:s2hms(clock:hms2s({H, M, S}))).
