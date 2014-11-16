3bccc39fee30604bf965174d0d3f5721f5fe2552
Mirko Bonadei <mirko.bonadei@gmail.com>
2014-11-16 10:39:49 +0100

    Completed clock:hms2s/1 tests
    Also tested the inverse only trusting that clock:hms2s/1 is right (round trip property)

--- test/clock_tests.erl
+++ test/clock_tests.erl
@@ -17,14 +17,69 @@
 time_dom() ->
   {choose(0, 23), choose(0, 59), choose(0, 59)}.
 
+%% Generates a time between 00:00:00 and 3:09:09.
+%% It olny needs to create small deltas without having to use 
+%% random:uniform/1 inside the property because in that case 
+%% Quickcheck does not shrinks it. (used below)
+delta_dom() ->
+  {choose(0, 3), choose(0, 9), choose(0, 9)}.  
+
 %% Properties
-prop_to_seconds_conversion_always_greater_than_0() ->
+prop_time_to_seconds_conversion_always_greater_than_0() ->
   ?FORALL(
-     {H, M, S},
-     time_dom(),
+    {H, M, S},
+    time_dom(),
        clock:hms2s({H, M, S}) >= 0).
 
-prop_to_seconds_conversion_inverse() ->
+prop_an_hour_weights_3600_seconds() ->
+  ?FORALL(
+    {H, M, S},
+    time_dom(),
+  ?IMPLIES(H < 23,
+       begin
+         Seconds1 = clock:hms2s({H, M, S}),
+         Seconds2 = clock:hms2s({H + 1, M, S}),
+         Seconds1 =:= Seconds2 - 3600
+       end)).
+
+prop_a_minute_weights_60_seconds() ->
+  ?FORALL(
+    {H, M, S},
+    time_dom(),
+  ?IMPLIES(M < 59,
+      begin
+         Seconds1 = clock:hms2s({H, M, S}),
+         Seconds2 = clock:hms2s({H, M + 1, S}),
+         Seconds1 =:= Seconds2 - 60
+      end)).
+
+prop_a_second_weights_1_second() ->
+  ?FORALL(
+    {H, M, S},
+    time_dom(),
+  ?IMPLIES(S < 59,
+      begin
+         Seconds1 = clock:hms2s({H, M, S}),
+         Seconds2 = clock:hms2s({H, M, S + 1}),
+         Seconds1 =:= Seconds2 - 1
+      end)).
+
+%% This property is difficult to write because I are not managing 
+%% time overflow. We start from 00:00:00 and stop to 23:59:59, so that 
+%% I have to find a trusted delta to sum to the original time.
+prop_time_to_seconds_is_monotonically_increasing() ->
+  ?FORALL(
+    [{H, M, S}, {Hdelta, Mdelta, Sdelta}],
+    [time_dom(), delta_dom()],
+  ?IMPLIES(H < 20,
+  ?IMPLIES(M < 50,
+  ?IMPLIES(S < 50,
+    begin
+      Time2 = {H + Hdelta, M + Mdelta, S + Sdelta},
+      clock:hms2s({H, M, S}) =< clock:hms2s(Time2)
+    end)))).
+
+prop_time_to_seconds_conversion_inverse() ->
   ?FORALL(
      {H, M, S},
      time_dom(),
